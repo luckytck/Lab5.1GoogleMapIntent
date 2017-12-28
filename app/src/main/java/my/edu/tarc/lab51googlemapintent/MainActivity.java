770433917e42b179e54a7e055ea74d6c148baf43
Tan Cheong Kiat <luckytck97@gmail.com>
2017-12-28 13:44:00 +0800

    week11

---
+++ app/src/main/java/my/edu/tarc/lab51googlemapintent/MainActivity.java
@@ -0,0 +1,37 @@
+package my.edu.tarc.lab51googlemapintent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editTextLat, editTextLon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextLat = (EditText)findViewById(R.id.editTextLat);
+        editTextLon = (EditText)findViewById(R.id.editTextLon);
+
+        Button buttonShowMap = (Button)findViewById(R.id.buttonShowMap);
+
+        buttonShowMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double lat,lon;
+                lat = Double.parseDouble(editTextLat.getText().toString());
+                lon = Double.parseDouble(editTextLon.getText().toString());
+
+                Uri gmmIntentUri = Uri.parse("geo:"+lat+","+lon+"?z=18,"+"q=McDonald");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                startActivity(mapIntent);
+            }
+        });
+    }
+}
